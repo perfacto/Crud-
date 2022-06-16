ac9e46e6c2bdfe0e9643c72fa0124e46673de031
shubham kumar <31152885+perfacto@users.noreply.github.com>
2022-06-16 11:37:32 +0530

    Add files via upload

---
+++ Api-gateway/src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity security) throws Exception {
+	security.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+	.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+	}
+
+}
